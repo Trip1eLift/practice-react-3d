d206857fd1de8bd0c64be6b0f96c4fa159352762
Joseph C <43707388+Trip1eLift@users.noreply.github.com>
2021-10-24 19:18:21 -0400

    Followed a tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+import { Physics, useBox, usePlane } from '@react-three/cannon';
+import './styles.css';
 
-function App() {
+// Tutorial: https://www.youtube.com/watch?v=9ZEjSxDRIik
+
+function Box() {
+  const [ref, api] = useBox(() => ({
+    mass: 1,
+    position: [0, 2, 0]
+  }));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <mesh onClick={() => {
+      api.velocity.set(0, 2, 0);
+    }} ref={ref} position={[0, 2, 0]}>
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  )
 }
 
-export default App;
+function Plane() {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+
+  }));
+  return (
+    <mesh ref={ref} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <boxBufferGeometry attach="geometry" args={[100, 100, 0]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  )
+}
+
+export default function App() {
+  return (
+    <>
+      <div>
+        Hello World!
+      </div>
+      <Canvas>
+        <OrbitControls />
+        <Stars/>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 15, 10]} angle={0.3} />
+        <Physics>
+          <Plane />
+          <Box/>
+        </Physics>
+        
+      </Canvas>
+    </>
+  );
+}
